ae759d6d888a8b3ac99977bd7813ebd741b9422e
Weezer Su <weezer.su@gmail.com>
2016-08-17 22:06:13 -0500

    shellsort and fix insertion_sort

--- insertionSort.py
+++ insertionSort.py
@@ -5,8 +5,9 @@ def insertionSort(alist):
             temp = alist[position]
             alist[position] = alist[position-1]
             alist[position-1] = temp
+            position -= 1
     print alist
 
-alist = [3]
+alist = [12, 43, 152, 1, 2, 4, 11, 35, 22, 23, 5, 666]
 
 insertionSort(alist)
--- shellSort.py
+++ shellSort.py
@@ -1,11 +1,26 @@
 #TODO finish the shell sort
-def shellSort(alist):
-    gap = len(alist) // 2
-    while gap > 0:
-        for index in range(gap):
-            while index < len(alist) and alist[index - gap] > alist[index]:
-                temp = alist[index - gap]
-                alist[index - gap] = alist[index]
-                alist[index] = temp
-        gap = gap // 2
+def shellsort(alist):
+    sublistgap = len(alist) // 2
+    while sublistgap >= 1:
+        for i in range(0, sublistgap):
+            insertion_sort(alist, i, sublistgap)
+        sublistgap //= 2
+        print alist
 
+
+def insertion_sort(thelist, start, gap):
+    listlen = len(thelist)
+    for i in range(start, listlen, gap):
+        position = i
+        while position > 0 and thelist[position - gap] > thelist[position]:
+            temp = thelist[position]
+            thelist[position] = thelist[position - gap]
+            thelist[position - gap] = temp
+            position -= gap
+
+
+xxx = [12, 43, 152, 1, 2, 4, 11, 35, 22, 23, 5, 666]
+
+
+shellsort(xxx)
+# print xxx
